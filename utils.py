ddcdd18b96b33d095633e39f2664753aca7a9ab0
babaozhouy5 <1609032563@qq.com>
2017-05-14 16:50:12 +0800

    add utils

---
+++ utils.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+    func: this is utils set package
+'''
+
+import pinyin
+
+zh_dict = (u'中', u'中国', u'中国人')
+
+def getPinYin(w1):
+    return pinyin.get(w1, format='strip')
+
+def levenshtein(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
